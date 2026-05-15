225fc2d3d124bf6199077151c6e96b091606da03
Sabonete190 <gilvanamaral2004@gmail.com>
2026-05-15 02:46:03 -0300

    app.py 1

--- app.py
+++ app.py
@@ -629,51 +629,6 @@ if st.button("Analisar Jogo"):
         f"Kelly BTTS NÃO: "
         f"{round(kelly_btts_nao * 100, 2)}%"
     )
-    # =========================
-    # TOP APOSTA
-    # =========================
-
-    st.subheader("Top Aposta do Jogo")
-
-    mercados = {
-
-        "Vitória Casa": edge_casa,
-        "Empate": edge_empate,
-        "Vitória Fora": edge_fora,
-
-        "Over 2.5": edge_over25,
-        "Under 2.5": edge_under25,
-
-        "BTTS SIM": edge_btts_sim,
-        "BTTS NÃO": edge_btts_nao
-    }
-
-    melhor_mercado = max(
-        mercados,
-        key=mercados.get
-    )
-
-    melhor_edge_final = mercados[
-        melhor_mercado
-    ]
-
-    if melhor_edge_final > 0:
-
-        st.success(
-            f"🔥 Melhor Aposta: "
-            f"{melhor_mercado}"
-        )
-
-        st.write(
-            f"Edge: "
-            f"{round(melhor_edge_final * 100, 2)}%"
-        )
-
-    else:
-
-        st.error(
-            "❌ Nenhuma aposta de valor encontrada"
-        )
  # =========================
     # PROBABILIDADES PRÓPRIAS
     # =========================
@@ -1116,3 +1071,48 @@ if st.button("Analisar Jogo"):
     st.success(
         f"{perfil_jogo}"
     )
+# =========================
+    # TOP APOSTA
+    # =========================
+
+    st.subheader("Top Aposta do Jogo")
+
+    mercados = {
+
+        "Vitória Casa": edge_casa,
+        "Empate": edge_empate,
+        "Vitória Fora": edge_fora,
+
+        "Over 2.5": edge_over25,
+        "Under 2.5": edge_under25,
+
+        "BTTS SIM": edge_btts_sim,
+        "BTTS NÃO": edge_btts_nao
+    }
+
+    melhor_mercado = max(
+        mercados,
+        key=mercados.get
+    )
+
+    melhor_edge_final = mercados[
+        melhor_mercado
+    ]
+
+    if melhor_edge_final > 0:
+
+        st.success(
+            f"🔥 Melhor Aposta: "
+            f"{melhor_mercado}"
+        )
+
+        st.write(
+            f"Edge: "
+            f"{round(melhor_edge_final * 100, 2)}%"
+        )
+
+    else:
+
+        st.error(
+            "❌ Nenhuma aposta de valor encontrada"
+        )
