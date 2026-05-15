09a208aeee83d30d41c0bfe987dca15ffea34e1d
Sabonete190 <gilvanamaral2004@gmail.com>
2026-05-15 02:18:06 -0300

    Atualizando interface

--- app.py
+++ app.py
@@ -840,78 +840,45 @@ if st.button("Analisar Jogo"):
         f"{round(edge_fora * 100, 2)}%"
     )
 # =========================
-    # EDGE
+    # EDGE 1X2
     # =========================
 
     edge_casa = (
-        prob_casa_modelo - prob_casa
+
+        prob_casa_modelo -
+
+        (1 / odd_casa)
     )
 
     edge_empate = (
-        prob_empate_modelo - prob_empate
+
+        prob_empate_modelo -
+
+        (1 / odd_empate)
     )
 
     edge_fora = (
-        prob_fora_modelo - prob_fora
+
+        prob_fora_modelo -
+
+        (1 / odd_fora)
     )
 
-    st.subheader("Edge do Modelo")
+    st.subheader("Edge 1X2")
 
     st.write(
-        f"Edge Casa: {round(edge_casa * 100, 2)}%"
-    )
-
-    st.write(
-        f"Edge Empate: {round(edge_empate * 100, 2)}%"
-    )
-
-    st.write(
-        f"Edge Fora: {round(edge_fora * 100, 2)}%"
-    )
-    # =========================
-    # KELLY CRITERION
-    # =========================
-
-    def calcular_kelly(probabilidade, odd):
-
-        kelly = (
-            (
-                odd * probabilidade
-            ) - 1
-        ) / (odd - 1)
-
-        return max(kelly, 0)
-
-    kelly_casa = calcular_kelly(
-        prob_casa_modelo,
-        odd_casa
-    )
-
-    kelly_empate = calcular_kelly(
-        prob_empate_modelo,
-        odd_empate
-    )
-
-    kelly_fora = calcular_kelly(
-        prob_fora_modelo,
-        odd_fora
-    )
-
-    st.subheader("Kelly Criterion")
-
-    st.write(
-        f"Kelly Casa: "
-        f"{round(kelly_casa * 100, 2)}%"
+        f"Edge Casa: "
+        f"{round(edge_casa * 100, 2)}%"
     )
 
     st.write(
-        f"Kelly Empate: "
-        f"{round(kelly_empate * 100, 2)}%"
+        f"Edge Empate: "
+        f"{round(edge_empate * 100, 2)}%"
     )
 
     st.write(
-        f"Kelly Fora: "
-        f"{round(kelly_fora * 100, 2)}%"
+        f"Edge Fora: "
+        f"{round(edge_fora * 100, 2)}%"
     )
     # =========================
     # CONFIANÇA DO MODELO
