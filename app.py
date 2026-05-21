716a40ecb0f9e09eae543a0c51413f3966894b29
Sabonete190 <gilvanamaral2004@gmail.com>
2026-05-20 22:27:34 -0300

    Update app.py 29

--- app.py
+++ app.py
@@ -1235,6 +1235,48 @@ if st.button("Analisar Jogo"):
     st.success(
         f"{perfil_jogo}"
     )
+    # =========================
+# MELHOR MERCADO
+# =========================
+
+melhor_mercado = "Nenhum"
+
+melhor_ev = max(
+    ev_casa,
+    ev_empate,
+    ev_fora,
+    ev_over25,
+    ev_under25,
+    ev_btts_sim,
+    ev_btts_nao
+)
+
+if melhor_ev == ev_casa:
+    melhor_mercado = "Casa"
+
+elif melhor_ev == ev_empate:
+    melhor_mercado = "Empate"
+
+elif melhor_ev == ev_fora:
+    melhor_mercado = "Fora"
+
+elif melhor_ev == ev_over25:
+    melhor_mercado = "Over 2.5"
+
+elif melhor_ev == ev_under25:
+    melhor_mercado = "Under 2.5"
+
+elif melhor_ev == ev_btts_sim:
+    melhor_mercado = "BTTS SIM"
+
+elif melhor_ev == ev_btts_nao:
+    melhor_mercado = "BTTS NÃO"
+
+st.subheader("Melhor Mercado")
+
+st.success(
+    f"🔥 {melhor_mercado}"
+)
 # =========================
     # TOP APOSTA
     # =========================
@@ -1280,6 +1322,23 @@ if st.button("Analisar Jogo"):
         st.error(
             "❌ Nenhuma aposta de valor encontrada"
         )
+        # =========================
+# SALVAR RESULTADOS
+# =========================
+
+st.session_state["melhor_mercado"] = melhor_mercado
+
+st.session_state["ev_casa"] = ev_casa
+st.session_state["ev_empate"] = ev_empate
+st.session_state["ev_fora"] = ev_fora
+
+st.session_state["edge_casa"] = edge_casa
+st.session_state["edge_empate"] = edge_empate
+st.session_state["edge_fora"] = edge_fora
+
+st.session_state["stake"] = stake
+st.session_state["confianca"] = confianca
+st.session_state["perfil_jogo"] = perfil_jogo
 
 # =========================
 # SALVAR APOSTA
